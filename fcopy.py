40f6def6ccaa278ae5f214883848fb8f64b90ea8
DukhDmytro <cowboybebop4991@gmail.com>
2019-12-02 10:51:46 +0200

    1st commit

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,9 @@
+"""module docstring"""
+
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """read line by line from file1 and write line by line to file2"""
+    with open(filename1, "rt") as old_file:
+        with open(filename2, "wt") as new_file:
+            for line in old_file:
+                new_file.write(line)
