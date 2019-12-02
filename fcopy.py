c754d2e0d00ef0a9ec10faba5967a737a9a69fe8
koreyo <KoretsValentyn@gmail.com>
2019-12-02 17:06:58 +0200

    cop

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,9 @@
+"""docstring"""
+
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """read information from file1 and write to file2"""
+    file_read = open(filename1)
+    file_write = open(filename2, 'w')
+    for i in file_read:
+        file_write.write(i)
