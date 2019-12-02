a97a704a66f00309abfc518822e57c3017672593
ValentinaLV <tina.lysenok@gmail.com>
2019-12-02 18:13:54 +0200

    added alternative file_copy method

--- fcopy.py
+++ fcopy.py
@@ -13,3 +13,14 @@ def fcopy(filename1: str, filename2: str) -> None:
     with open(filename1, "rt") as file_1:
         with open(filename2, "wt") as file_2:
             file_2.write(file_1.read())
+
+
+def file_copy(filename1: str, filename2: str) -> None:
+    """
+    Copy the contents of the file to a new file.
+    :param filename1: str
+    :param filename2: str
+    :return: None
+    """
+    with open(filename1, "rt") as f1, open(filename2, "wt") as f2:
+        f2.write(f1.read())
