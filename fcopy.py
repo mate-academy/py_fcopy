255d5f2fd370a03e88a411bd0e4104e602a98bef
d1makov <makovskyj.danylo@gmail.com>
2019-12-02 16:11:01 +0200

    final

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,16 @@
+"""
+docstring
+"""
+
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """
+
+    :param filename1:
+    :param filename2:
+    :return:
+    """
+    with open(filename1) as file1:
+        lines = file1.readlines()
+        with open(filename2, "w") as file2:
+            file2.writelines(lines)
