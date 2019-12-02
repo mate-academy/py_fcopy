30874a5d3fc450ec15ba129589af08d3633f32f9
ValentinaLV <tina.lysenok@gmail.com>
2019-12-02 11:59:30 +0200

    hw11_py_file_copy

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,15 @@
+"""
+module file copy
+"""
+
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """
+    Copy the contents of the file to a new file.
+    :param filename1: str
+    :param filename2: str
+    :return: None
+    """
+    with open(filename1, "rt") as file_1:
+        with open(filename2, "wt") as file_2:
+            file_2.write(file_1.read())
