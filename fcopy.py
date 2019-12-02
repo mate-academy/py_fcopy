cefab26e5136e1e2e3209f2f3068678eea67f788
Artem <artger@ukr.net>
2019-12-02 12:44:04 +0200

    version 1

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,16 @@
-def fcopy(filename1: str, filename2: str) -> None:
-    pass
+'''
+Module
+'''
+
+
+def fcopy(read_file: str, write_file: str) -> None:
+    '''
+    Read text from read_file adn write this text in write_file
+    :param read_file:
+    :param write_file:
+    :return:
+    '''
+    with open(read_file, "rt") as reading:
+        with open(write_file, "wt") as writing:
+            for line in reading:
+                writing.write(line)
