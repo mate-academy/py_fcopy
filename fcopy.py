b1e4d8f8da14bc33f17b1ccf3da036b3a41e4d14
Senku1 <morhaliuk@gmail.com>
2019-12-02 09:45:04 +0200

    Solved

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,21 @@
+"""
+fcopy module
+Functions:
+    fcopy(filename1, filename2) -- copy content
+of filename1 file to filename2.
+"""
+
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """
+    Copy content of the file filename1 to
+    file filename2
+    :param filename1: str
+    :param filename2: str
+    :return: None
+    """
+    with open(filename1, 'r') as file:
+        context = file.read()
+
+    with open(filename2, 'w') as file:
+        file.write(context)
