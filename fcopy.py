d670b3d3bf19249ebe73301417ba4ea3e51f8765
logotip <logotip123@yahoo.com>
2019-12-02 10:19:28 +0200

    v 1.0

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,11 @@
+"""Copy file module"""
+
 def fcopy(filename1: str, filename2: str) -> None:
-    pass
+    """
+    :param filename1: Open filename1 for reading
+    :param filename2: Open filename2 for writing
+    :return: None
+    """
+    with open(filename1, 'rt') as file_1:
+        with open(filename2, 'wt') as file_2:
+            file_2.write(file_1.read())
