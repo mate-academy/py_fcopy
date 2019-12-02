ccf5775f51444a9f7d07352e09c6a5d69fcdf5ad
Dima <dima.sapsay@gmail.com>
2019-12-02 15:41:11 +0200

    first commit

--- fcopy.py
+++ fcopy.py
@@ -6,8 +6,7 @@ Copy the contents of the file to a new file
 def fcopy(filename1: str, filename2: str) -> None:
     """Copy the contents in a new file"""
     with open(filename1, 'rt') as rfile:
-        with open(filename1, 'rt') as rfile:
-            strg = rfile.read()
+        strg = rfile.read()
 
-        with open(filename2, 'wt') as wfile:
-            wfile.write(strg)
+    with open(filename2, 'wt') as wfile:
+        wfile.write(strg)
