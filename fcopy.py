cc2a136993fdc7d275fbab11051015010e48a43c
Andrii <andrewsotnikov88@gmail.com>
2019-12-05 09:11:52 +0200

    Function to copy the content of one file to another

--- fcopy.py
+++ fcopy.py
@@ -1,2 +1,10 @@
-def fcopy(filename1: str, filename2: str) -> None:
-    pass
+"""Copy the contents of the file to a new file."""
+
+
+def fcopy(first_file_name: str, second_file_name: str) -> str:
+    """Function to copy the content of one file to another """
+    with open(first_file_name, 'r') as file1:
+        content_to_copy = file1.read()
+    with open(second_file_name, 'w') as file2:
+        file2.write(content_to_copy)
+    return content_to_copy
